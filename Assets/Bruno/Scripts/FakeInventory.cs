4aa279abd36593b6d4db21d9f7b2b1704c6ca45e
BPA98 <bpaone.dev@gmail.com>
2025-02-16 18:28:25 +0100

    lighthouse v3
    add lighthouse script, battery script and check win condition

--- Assets/Bruno/Scripts/FakeInventory.cs
+++ Assets/Bruno/Scripts/FakeInventory.cs
@@ -9,29 +9,27 @@ namespace Bruno.Scripts
     {
         [SerializeField] private RawImage ui;
         [SerializeField] private TMP_Text textCounter;
-        private int _mBulbCounter = 0;
+        private static int mBulbCounter = 0;
+        public static bool WinConditionReached => WinCondition();
 
         public static bool itemPicked { get; set; } = false;
 
         private void Start()
         {
-            textCounter.text = _mBulbCounter.ToString();
+            textCounter.text = mBulbCounter.ToString();
         }
         
         // itemPicked will set to true via player OnTriggerEnter function with the relative tag 
         public void GetItem()
         {
-            _mBulbCounter++;
-            textCounter.text = _mBulbCounter.ToString();
+            mBulbCounter++;
+            textCounter.text = mBulbCounter.ToString();
             itemPicked = false;
         }
 
-        public void CheckWinCondition()
+        private static bool WinCondition()
         {
-            if (_mBulbCounter >= 2)
-            {
-                //TODO lighthouse will call the boat
-            }
+            return mBulbCounter >= 2;
         }
     
     }
